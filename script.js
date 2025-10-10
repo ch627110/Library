5f14d6ce632e8b4641f2ff2862f785f4d11df3f9
Chris <133912058+ch627110@users.noreply.github.com>
2025-10-09 20:00:05 -0400

    Create functionality to display book info

--- script.js
+++ script.js
@@ -1,30 +1,60 @@
 const myLibrary = [];
 
-function Book(id, title, author, numPages, haveRead) {
+function Book(title, author, numPages, haveRead) {
     // the constructor
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor")
     }
-    this.id = id;
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.numPages = numPages;
     this.haveRead = haveRead;
+    this.displayBook = function() {
+        console.log('Book ID: '+this.id)
+        console.log('Book Title: '+this.title)
+        console.log('Book Author: '+this.author)
+        console.log('Number of Pages: '+this.numPages)   
+        console.log('Read Book: '+this.haveRead)        
+    }
 }
 
-function addBookToLibrary() {
-    let id = crypto.randomUUID();
-    let title;
-    let author;
-    let numPages;
-    let haveRead;
-    myLibrary.push(new Book(id, title, author, numPages, haveRead));
+function addBook(title, author, numPages, haveRead) { 
+    const book = new Book(title, author, numPages, haveRead)
+    myLibrary.push(book)
+    return book    
 }
 
-function displayEachBook() {
-    // write a function that loops through the array and displays each book
+(() => {
+    const the_hobbit = addBook('The Hobbit', 'JRR Tolkien', 300, false)
+    const pride_and_prejudice = addBook('Pride and Prejudice', 'Jane Austen', 480, 
+    false)
+    const animal_farm = addBook('Animal Farm', 'George Orwell', 141, true)
+    display()
+})();
+
+function display() {
+    const books_container = document.querySelector('#books-container')
     myLibrary.forEach((book) => {
-        console.log(book)
+        const book_div = document.createElement('div')
+        book_div.classList.add('book-div')
+
+        const book_div_title = document.createElement('p')
+        const book_div_author = document.createElement('p')
+        const book_div_pages = document.createElement('p')
+        const book_div_read = document.createElement('p')
+
+        book_div_title.textContent = ('Book title: '+book.title)
+        book_div_author.textContent = ('Book author: '+book.author)
+        book_div_pages.textContent = ('Number of pages: '+book.numPages)
+        book_div_read.textContent = ('Read status: '+book.haveRead)
+
+        book_div.appendChild(book_div_title)
+        book_div.appendChild(book_div_author)
+        book_div.appendChild(book_div_pages)
+        book_div.appendChild(book_div_read)
+
+        books_container.appendChild(book_div)
     })
 }
 
@@ -35,9 +65,4 @@ const closeButton = document.querySelector('dialog button')
 // open 'add a book' modal
 showButton.addEventListener('click', () => {
     dialog.showModal()
-})
-
-// submit modal
-close.addEventListener('click', () => {
-    dialog.close()
-})
+});
